2dcf804eafda84d5c9ac1c1dca6a28f7a34a2620
Dan Huang <github@yuzeh.com>
2019-04-21 17:35:15 -0700

    Add scripts

---
+++ scripts/run-2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+export WEBSOCKET_URI=localhost:8000
+export PS_USERNAME=abut-shipful-2
+export BOT_MODE=SEARCH_LADDER
+export POKEMON_MODE=gen7ou
+export TEAM_NAME=2_psyspam
+export RUN_COUNT=200
+
+python run.py
---
+++ scripts/run-3.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+export WEBSOCKET_URI=localhost:8000
+export PS_USERNAME=abut-shipful-3
+export BOT_MODE=SEARCH_LADDER
+export POKEMON_MODE=gen7ou
+export TEAM_NAME=3_trickroom
+export RUN_COUNT=200
+
+python run.py
